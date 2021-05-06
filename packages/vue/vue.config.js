fc4725ebd4fd6791bc98cb89daa9d65c54121acb
Vlad Volkov <vlad.volkov@bndigital.co>
2021-05-06 21:36:29 +0300

    Added vue 3 typescript template

---
+++ packages/vue/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  lintOnSave: false,
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule("svg");
+    svgRule.uses.clear();
+    svgRule
+      .use("vue-loader")
+      .loader("vue-loader")
+      .end()
+      .use("vue-svg-loader")
+      .loader("vue-svg-loader");
+  },
+}
