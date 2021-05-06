aeefc03e10878a135903de93721c88f9cb780b00
Vlad Volkov <vlad.volkov@bndigital.co>
2021-05-06 04:24:21 +0300

    Fixed actions

--- packages/website/src/index.tsx
+++ packages/website/src/index.tsx
@@ -3,6 +3,5 @@ import { App } from './containers'
 import reportWebVitals from './reportWebVitals'
 
 const rootElement = window.document.getElementById('root')
-
 render(<App />, rootElement)
 reportWebVitals()
