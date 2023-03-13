9a0ef6a0b36a8481e066100854d6e570b8db6295
Vlad Volkov <vlad.volkov@bndigital.co>
2023-03-13 13:35:22 +0200

    Fixed website middleware

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,14 +2,14 @@ import { Projects } from 'projen'
 import { FullStackProjectOptions } from '@bn-digital/projen'
 
 Projects.createProject({
-  dir: `${__dirname}/test`,
+  dir: `${__dirname}/app`,
   post: false,
   projectFqn: '@bn-digital/projen.FullStackProject',
   projectOptions: {
-    name: 'test',
-    defaultReleaseBranch: 'latest',
-    react: true,
-    graphql: true,
-    strapi: true,
+    ide: { editorconfig: true },
+    linters: { prettier: true, eslint: true, stylelint: true },
+    react: { version: '18.2.0', antd: true, router: true, graphql: true, i18n: false },
+    graphql: { codegen: true },
+    strapi: { database: { client: 'postgres' }, users: { enabled: true } },
   } as FullStackProjectOptions,
 })
--- packages/cms/src/middlewares/website.ts
+++ packages/cms/src/middlewares/website.ts
@@ -1,30 +1,52 @@
-import * as fs from 'fs'
 import { Context, Next } from 'koa'
-import path from 'path'
-import { VoidFunctionComponent } from 'react'
 
-import app from '../hooks'
-
-const STRAPI_URL_REGEXP =
-  /\/(admin|api|graphql|content-type-builder|content-manager|users-permissions|import-export-entries|config-sync|i18n|upload|email-designer)(.*)/
-const ASSET_URL_REGEXP = /^\/(assets|sitemap|uploads)\/(.*).(js|css|ttf|woff|jpg|jpeg|bmp|svg|gif|tiff|mp4|mov|pdf)/i
-
-/**
- * TODO(vlad): This is a temporary solution to serve the SPA.
- */
-export default () => (ctx: Context, next: () => void) => {
+const strapiRoutes = [
+  'admin',
+  'api',
+  'assets',
+  'config-sync',
+  'content-manager',
+  'content-type-builder',
+  'email',
+  'email-designer',
+  'favicon',
+  'graphql',
+  'i18n',
+  'import-export-entries',
+  'sitemap',
+  'transfer',
+  'upload',
+  'uploads',
+  'users-permissions',
+]
+const extensions = [
+  'js',
+  'css',
+  'ttf',
+  'woff',
+  'jpg',
+  'jpeg',
+  'bmp',
+  'png',
+  'ico',
+  'svg',
+  'gif',
+  'tiff',
+  'mp4',
+  'mov',
+  'pdf',
+]
+export default () => (ctx: Context, next: Next) => {
   if (
     ctx.method.toLowerCase() === 'get' &&
-    !ctx.URL.pathname.match(STRAPI_URL_REGEXP) &&
-    !ctx.URL.pathname.match(ASSET_URL_REGEXP)
+    !ctx.URL.pathname.match(new RegExp(`/(${strapiRoutes.join('|')})(.*).(${extensions.join('|')})`))
   ) {
     if (ctx.response.status === 404 || ctx.url === '/') {
       ctx.url = '/index.html'
       ctx.type = 'html'
-      ctx.body = fs.createReadStream(path.join(app.workingDir, 'public', 'index.html'))
-
       return next()
     }
+  } else {
+    return next()
   }
-  return next()
 }
