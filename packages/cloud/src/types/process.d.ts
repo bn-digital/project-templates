b23339d6d466fb84fd763c74b428d0a3b4f7fc8b
Vlad Volkov <vlad.volkov@bndigital.co>
2023-05-08 16:35:40 +0300

    fix: documentation

--- packages/cloud/src/types/process.d.ts
+++ packages/cloud/src/types/process.d.ts
@@ -3,11 +3,16 @@ declare global {
     interface ProcessEnv {
       /**
        * Used by Pulumi to store state files in S3 bucket.
-       * Assumes that aws profile `digitalocean` is configured with credentials from BN Digitalocean.
        * @example s3://bn-digital/pulumi/project-templates?endpoint=fra1.digitaloceanspaces.com&profile=digitalocean"
        */
       PULUMI_BACKEND_URL: string
       PULUMI_CONFIG_PASSPHRASE: string
+      /**
+       * Pulumi S3 backend require AWS-like credentials to access S3 bucket no matter which cloud provider is used.
+       */
+      AWS_SECRET_ACCESS_KEY: string
+      AWS_ACCESS_KEY_ID: string
+      AWS_REGION: string
       /**
        * Used in staging environment to configure Vault provider and fetch secrets from it
        */
@@ -25,16 +30,16 @@ declare global {
        */
       DIGITALOCEAN_TOKEN: string
       /**
-       * Used in production environment to create DigitalOcean Spaces bucket
+       * Used in production environment to manage DigitalOcean Spaces bucket
        */
       SPACES_ACCESS_KEY_ID: string
       SPACE_SECRET_ACCESS_KEY: string
       /**
-       * Used in staging to set repository owner on Github Secrets population
+       * Used in staging to bind any changes in repository to organization by default
        */
       GITHUB_OWNER: string
       /**
-       * Used to setup Harbor provider and create container registry
+       * Used to setup Harbor provider and manage container registry
        */
       HARBOR_USERNAME: string
       HARBOR_PASSWORD: string
