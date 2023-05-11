5549024534f0b4992508e2397326953c224b5655
Vlad Volkov <vlad.volkov@bndigital.co>
2023-05-11 20:22:10 +0300

    fix: graphql code regenerated

--- packages/website/src/graphql/index.ts
+++ packages/website/src/graphql/index.ts
@@ -1,5 +1,5 @@
 import * as Apollo from "@apollo/client"
-import { DocumentNode } from "graphql"
+import { gql } from "@apollo/client"
 const defaultOptions = {} as const
 
 export type PossibleTypesResultData = {
@@ -50,253 +50,94 @@ const result: PossibleTypesResultData = {
 }
 export default result
 
-export const EntryFragmentDoc = {
-  kind: "Document",
-  definitions: [
-    {
-      kind: "FragmentDefinition",
-      name: { kind: "Name", value: "Entry" },
-      typeCondition: { kind: "NamedType", name: { kind: "Name", value: "ComponentDataEntry" } },
-      selectionSet: {
-        kind: "SelectionSet",
-        selections: [
-          { kind: "Field", name: { kind: "Name", value: "id" } },
-          { kind: "Field", name: { kind: "Name", value: "key" } },
-          { kind: "Field", name: { kind: "Name", value: "value" } },
-        ],
-      },
-    },
-  ],
-} as unknown as DocumentNode
-export const HeadlineFragmentDoc = {
-  kind: "Document",
-  definitions: [
-    {
-      kind: "FragmentDefinition",
-      name: { kind: "Name", value: "Headline" },
-      typeCondition: { kind: "NamedType", name: { kind: "Name", value: "ComponentUiHeadline" } },
-      selectionSet: {
-        kind: "SelectionSet",
-        selections: [
-          { kind: "Field", name: { kind: "Name", value: "id" } },
-          { kind: "Field", name: { kind: "Name", value: "title" } },
-          { kind: "Field", name: { kind: "Name", value: "subtitle" } },
-        ],
-      },
-    },
-  ],
-} as unknown as DocumentNode
-export const ParagraphFragmentDoc = {
-  kind: "Document",
-  definitions: [
-    {
-      kind: "FragmentDefinition",
-      name: { kind: "Name", value: "Paragraph" },
-      typeCondition: { kind: "NamedType", name: { kind: "Name", value: "ComponentUiParagraph" } },
-      selectionSet: {
-        kind: "SelectionSet",
-        selections: [
-          { kind: "Field", name: { kind: "Name", value: "id" } },
-          { kind: "Field", name: { kind: "Name", value: "value" } },
-        ],
-      },
-    },
-  ],
-} as unknown as DocumentNode
-export const LinkFragmentDoc = {
-  kind: "Document",
-  definitions: [
-    {
-      kind: "FragmentDefinition",
-      name: { kind: "Name", value: "Link" },
-      typeCondition: { kind: "NamedType", name: { kind: "Name", value: "ComponentUiLink" } },
-      selectionSet: {
-        kind: "SelectionSet",
-        selections: [
-          { kind: "Field", name: { kind: "Name", value: "id" } },
-          { kind: "Field", name: { kind: "Name", value: "title" } },
-          { kind: "Field", name: { kind: "Name", value: "url" } },
-          { kind: "Field", name: { kind: "Name", value: "target" } },
-        ],
-      },
-    },
-  ],
-} as unknown as DocumentNode
-export const FileFragmentDoc = {
-  kind: "Document",
-  definitions: [
-    {
-      kind: "FragmentDefinition",
-      name: { kind: "Name", value: "File" },
-      typeCondition: { kind: "NamedType", name: { kind: "Name", value: "UploadFileEntity" } },
-      selectionSet: {
-        kind: "SelectionSet",
-        selections: [
-          { kind: "Field", name: { kind: "Name", value: "id" } },
-          {
-            kind: "Field",
-            name: { kind: "Name", value: "attributes" },
-            selectionSet: {
-              kind: "SelectionSet",
-              selections: [
-                { kind: "Field", name: { kind: "Name", value: "previewUrl" } },
-                { kind: "Field", name: { kind: "Name", value: "alternativeText" } },
-                { kind: "Field", name: { kind: "Name", value: "url" } },
-              ],
-            },
-          },
-        ],
-      },
-    },
-  ],
-} as unknown as DocumentNode
-export const CardFragmentDoc = {
-  kind: "Document",
-  definitions: [
-    {
-      kind: "FragmentDefinition",
-      name: { kind: "Name", value: "Card" },
-      typeCondition: { kind: "NamedType", name: { kind: "Name", value: "ComponentUiCard" } },
-      selectionSet: {
-        kind: "SelectionSet",
-        selections: [
-          { kind: "Field", name: { kind: "Name", value: "id" } },
-          { kind: "Field", name: { kind: "Name", value: "title" } },
-          { kind: "Field", name: { kind: "Name", value: "subtitle" } },
-          { kind: "Field", name: { kind: "Name", value: "description" } },
-          {
-            kind: "Field",
-            name: { kind: "Name", value: "media" },
-            selectionSet: {
-              kind: "SelectionSet",
-              selections: [
-                {
-                  kind: "Field",
-                  name: { kind: "Name", value: "data" },
-                  selectionSet: {
-                    kind: "SelectionSet",
-                    selections: [{ kind: "FragmentSpread", name: { kind: "Name", value: "File" } }],
-                  },
-                },
-              ],
-            },
-          },
-        ],
-      },
-    },
-  ],
-} as unknown as DocumentNode
-export const SectionFragmentDoc = {
-  kind: "Document",
-  definitions: [
-    {
-      kind: "FragmentDefinition",
-      name: { kind: "Name", value: "Section" },
-      typeCondition: { kind: "NamedType", name: { kind: "Name", value: "ComponentUiSection" } },
-      selectionSet: {
-        kind: "SelectionSet",
-        selections: [
-          { kind: "Field", name: { kind: "Name", value: "id" } },
-          {
-            kind: "Field",
-            name: { kind: "Name", value: "button" },
-            selectionSet: {
-              kind: "SelectionSet",
-              selections: [{ kind: "FragmentSpread", name: { kind: "Name", value: "Link" } }],
-            },
-          },
-          {
-            kind: "Field",
-            name: { kind: "Name", value: "heading" },
-            selectionSet: {
-              kind: "SelectionSet",
-              selections: [{ kind: "FragmentSpread", name: { kind: "Name", value: "Card" } }],
-            },
-          },
-        ],
-      },
-    },
-  ],
-} as unknown as DocumentNode
-export const TabFragmentDoc = {
-  kind: "Document",
-  definitions: [
-    {
-      kind: "FragmentDefinition",
-      name: { kind: "Name", value: "Tab" },
-      typeCondition: { kind: "NamedType", name: { kind: "Name", value: "ComponentUiTab" } },
-      selectionSet: {
-        kind: "SelectionSet",
-        selections: [
-          { kind: "Field", name: { kind: "Name", value: "id" } },
-          { kind: "Field", name: { kind: "Name", value: "name" } },
-          {
-            kind: "Field",
-            name: { kind: "Name", value: "pane" },
-            selectionSet: {
-              kind: "SelectionSet",
-              selections: [{ kind: "FragmentSpread", name: { kind: "Name", value: "Card" } }],
-            },
-          },
-        ],
-      },
-    },
-  ],
-} as unknown as DocumentNode
-export const HomeDocument = {
-  kind: "Document",
-  definitions: [
-    {
-      kind: "OperationDefinition",
-      operation: "query",
-      name: { kind: "Name", value: "home" },
-      selectionSet: {
-        kind: "SelectionSet",
-        selections: [
-          {
-            kind: "Field",
-            name: { kind: "Name", value: "home" },
-            selectionSet: {
-              kind: "SelectionSet",
-              selections: [
-                {
-                  kind: "Field",
-                  name: { kind: "Name", value: "data" },
-                  selectionSet: {
-                    kind: "SelectionSet",
-                    selections: [
-                      {
-                        kind: "Field",
-                        name: { kind: "Name", value: "attributes" },
-                        selectionSet: {
-                          kind: "SelectionSet",
-                          selections: [
-                            {
-                              kind: "Field",
-                              name: { kind: "Name", value: "hero" },
-                              selectionSet: {
-                                kind: "SelectionSet",
-                                selections: [{ kind: "FragmentSpread", name: { kind: "Name", value: "Section" } }],
-                              },
-                            },
-                          ],
-                        },
-                      },
-                    ],
-                  },
-                },
-              ],
-            },
-          },
-        ],
-      },
-    },
-    ...SectionFragmentDoc.definitions,
-    ...LinkFragmentDoc.definitions,
-    ...CardFragmentDoc.definitions,
-    ...FileFragmentDoc.definitions,
-  ],
-} as unknown as DocumentNode
+export const EntryFragmentDoc = gql`
+  fragment Entry on ComponentDataEntry {
+    id
+    key
+    value
+  }
+`
+export const HeadlineFragmentDoc = gql`
+  fragment Headline on ComponentUiHeadline {
+    id
+    title
+    subtitle
+  }
+`
+export const ParagraphFragmentDoc = gql`
+  fragment Paragraph on ComponentUiParagraph {
+    id
+    value
+  }
+`
+export const LinkFragmentDoc = gql`
+  fragment Link on ComponentUiLink {
+    id
+    title
+    url
+    target
+  }
+`
+export const FileFragmentDoc = gql`
+  fragment File on UploadFileEntity {
+    id
+    attributes {
+      previewUrl
+      alternativeText
+      url
+    }
+  }
+`
+export const CardFragmentDoc = gql`
+  fragment Card on ComponentUiCard {
+    id
+    title
+    subtitle
+    description
+    media {
+      data {
+        ...File
+      }
+    }
+  }
+`
+export const SectionFragmentDoc = gql`
+  fragment Section on ComponentUiSection {
+    id
+    button {
+      ...Link
+    }
+    heading {
+      ...Card
+    }
+  }
+`
+export const TabFragmentDoc = gql`
+  fragment Tab on ComponentUiTab {
+    id
+    name
+    pane {
+      ...Card
+    }
+  }
+`
+export const HomeDocument = gql`
+  query home {
+    home {
+      data {
+        attributes {
+          hero {
+            ...Section
+          }
+        }
+      }
+    }
+  }
+  ${SectionFragmentDoc}
+  ${LinkFragmentDoc}
+  ${CardFragmentDoc}
+  ${FileFragmentDoc}
+`
 export function useHomeQuery(baseOptions?: Apollo.QueryHookOptions<HomeQuery, HomeQueryVariables>) {
   const options = { ...defaultOptions, ...baseOptions }
   return Apollo.useQuery<HomeQuery, HomeQueryVariables>(HomeDocument, options)
@@ -308,29 +149,13 @@ export function useHomeLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<HomeQ
 export type HomeQueryHookResult = ReturnType<typeof useHomeQuery>
 export type HomeLazyQueryHookResult = ReturnType<typeof useHomeLazyQuery>
 export type HomeQueryResult = Apollo.QueryResult<HomeQuery, HomeQueryVariables>
-export const MeDocument = {
-  kind: "Document",
-  definitions: [
-    {
-      kind: "OperationDefinition",
-      operation: "query",
-      name: { kind: "Name", value: "me" },
-      selectionSet: {
-        kind: "SelectionSet",
-        selections: [
-          {
-            kind: "Field",
-            name: { kind: "Name", value: "me" },
-            selectionSet: {
-              kind: "SelectionSet",
-              selections: [{ kind: "Field", name: { kind: "Name", value: "id" } }],
-            },
-          },
-        ],
-      },
-    },
-  ],
-} as unknown as DocumentNode
+export const MeDocument = gql`
+  query me {
+    me {
+      id
+    }
+  }
+`
 export function useMeQuery(baseOptions?: Apollo.QueryHookOptions<MeQuery, MeQueryVariables>) {
   const options = { ...defaultOptions, ...baseOptions }
   return Apollo.useQuery<MeQuery, MeQueryVariables>(MeDocument, options)
